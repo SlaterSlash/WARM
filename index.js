c4d5ba522ba6de9e52b06f93c96d85f9ef2f6c28
Ercadio <vroch@edu.uwaterloo.ca>
2017-04-25 15:13:52 -0400

    Added index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const PORT = 8080;
+app.use(express.static('./public'));
+
+app.listen(PORT,function(){
+  console.log('The server is up!');
+});
