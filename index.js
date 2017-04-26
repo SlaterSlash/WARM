f4c5cde57e77768941f0a3f937d080be11e07771
Ercadio <vroch@edu.uwaterloo.ca>
2017-04-25 20:16:06 -0400

    Added security

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
-app.use(express.static('./public'));
-
+app.use(express.static('./login'));
+app.post('/form', function(req, res){
+  console.log(req.headers);
+  res.end(JSON.stringify(req.body));
+});
 app.listen(PORT,function(){
-  console.log('The server is up!');
+  console.log('The server is up! Point your browser to localhost:' + PORT);
 });
---
+++ login/authentication.js
@@ -0,0 +1,4 @@
+var submitBtn = getElementById('submit_button');
+submitBtn.addEventListener('submit', function(e) {
+  console.log(e);
+});
