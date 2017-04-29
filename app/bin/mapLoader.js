00bda4c8743a208c628249080f7b5d1e1a21101b
Ercadio <vroch@edu.uwaterloo.ca>
2017-04-28 20:20:53 -0400

    modified:   app/bin/mapLoader.js 	deleted:    bin/ipAuthorizer.js 	deleted:    login/authentication.js 	deleted:    login/index.html

--- app/bin/mapLoader.js
+++ app/bin/mapLoader.js
@@ -10,7 +10,7 @@ window.addEventListener('load', function() {
 function loadMapChunk(lat,lon){
   var data = '';
   var linkRequest = new XMLHttpRequest();
-  linkRequest.open('GET','https://api.nasa.gov/planetary/earth/imagery?lon=' + lon + '&lat=' + lat + '&cloud_score=True&api_key=' + API_KEY);
+  linkRequest.open('GET','https://api.nasa.gov/planetary/earth/imagery?lon=' + lon + '&lat=' + lat + '&cloud_score=True&api_key=' + API_KEY,true);
   linkRequest.onreadystatechange = function(){
     console.log('linkRequest.readyState,linkRequest.status');
   }
--- login/authentication.js
+++
@@ -1,4 +0,0 @@
-var submitBtn = getElementById('submit_button');
-submitBtn.addEventListener('submit', function(e) {
-  console.log(e);
-});
