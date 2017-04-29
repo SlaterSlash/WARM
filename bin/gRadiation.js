5166366139260968b54a3d2385c6e8a3cd7687e1
Ercadio <vroch@edu.uwaterloo.ca>
2017-04-29 10:08:16 -0400

    modified:   bin/gRadiation.js 	modified:   index.js

--- bin/gRadiation.js
+++ bin/gRadiation.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-const GRAD_API_KEY = 'ASPSESSIONIDCQSBSCTD=PGCFGMEBHMCLNGEACHGNAIDJ';
-function init(http,express,app){
-  method = method || "post"; 
-  app.use('/grad',function(req,res){
-    const option = {
-      'host':'jag.cami.jccbi.gov',
-      'path':'/cariprofile2.asp?DateOfFlight=12%2F2016&Ocode=CYUL&Dcode=CYYZ&NumOfSteps=3&ClimbTime=10&StepAlt_1=10000&StepMin_1=15&StepAlt_2=15000&StepMin_2=20&StepAlt_3=10000&StepMin_3=10&MinDown=10&btnSubmit=Continue',
-      'headers':{
-       'username': GRAD_API_KEY
-      },
-      'method': 'POST',
-      'port': 80
-    };
-    http.request(option,function(response){
-=======
 function init(http,express,app){
   app.use('/grad',function(req,res){
     const post_data = 'DateOfFlight=12%2F2016&Ocode=KDYS&DCode=EGLL&NumOfSteps=2&ClimbTime=10&btnSubmit=Continue';
@@ -32,7 +16,6 @@ function init(http,express,app){
       }
     };
     var post_request = http.request(option,function(response){
->>>>>>> 6d8b278129516f5c02dc0ad41c74fe09fd361fe0
       var rspData = ''
       response.on('data',function(chunk){
         rspData += chunk;
@@ -40,13 +23,9 @@ function init(http,express,app){
       response.on('end',function(){
         res.end(rspData);
       });
-<<<<<<< HEAD
-    }).end();
-=======
     });
     post_request.write(post_data);
     post_request.end();
->>>>>>> 6d8b278129516f5c02dc0ad41c74fe09fd361fe0
   });
 }
 
