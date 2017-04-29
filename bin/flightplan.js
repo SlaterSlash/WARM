ba8ab39f720c8737a0c930193b8820ee80d1b5d4
SlaterSlash <slater_johnny@hotmail.com>
2017-04-29 17:16:56 -0400

    modified:   bin/flightplan.js

--- bin/flightplan.js
+++ bin/flightplan.js
@@ -9,6 +9,11 @@ app.use('/flight',function(req,res){
     'path':'/plan/' + req.query.flightID,
     'headers':{
       'username': key['rRoute']
+      'Content-Type': 'application/json; charset=utf-8',
+      'X-API-Version': 1,
+      'X-Units': 'SI',
+      'X-Limit-Cap': 100,
+      'X-Limit-Used': 1
     }
   };
   https.request(option3,function(response){
