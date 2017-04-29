fe17cce9baf365b0de21367f5b7d508e9928db60
SlaterSlash <slater_johnny@hotmail.com>
2017-04-29 16:57:32 -0400

    modified:   bin/rRoute.js

--- bin/rRoute.js
+++ bin/rRoute.js
@@ -3,9 +3,14 @@ function init(https,express,app){
   app.use('/fpdb',function(req,res){
     const option = {
       'host':'api.flightplandatabase.com',
-      'path':'/plan/' + req.query.flighID,
+      'path':'/plan/' + req.query.flightID,
       'headers':{
-        'username': FPDB_API_KEY
+        'username': FPDB_API_KEY,
+        'Content-Type': 'application/json; charset=utf-8',
+        'X-API-Version': 1,
+        'X-Units': 'SI',
+        'X-Limit-Cap': 100,
+        'X-Limit-Used': 1
       }
     };
     https.request(option,function(response){
