dba0f129b6e3bb66351466828193f8d265873e2c
Ercadio <vroch@edu.uwaterloo.ca>
2017-04-28 21:42:17 -0400

    modified:   app/bin/mapLoader.js 	new file:   bin/nasaAPI.js 	modified:   index.js

---
+++ bin/nasaAPI.js
@@ -0,0 +1,17 @@
+module.export = function (){
+  app.get('/nasa',function(req,res){
+    const option = {
+      'host':'api.nasa.gov',
+      'path':'/planetary/earth/imagery?lon=' + req.query.lon + '&lat=' + req.query.lat + '&cloud_score=True&api_key=' + NASA_API_KEY
+    };
+    https.request(option,function(response){
+      var rspData = ''
+      response.on('data',function(chunk){
+        rspData += chunk;
+      });
+      response.on('end',function(){
+        res.end(JSON.parse(rspData)['url']);
+      })
+    }).end();
+  });
+}
--- index.js
+++ index.js
@@ -3,26 +3,12 @@ const app = express();
 const fs = require('fs');
 const https = require('https');
 const PORT = 8080;
-
+const nasaAPI = require('nasaAPI');
 const NASA_API_KEY = 'KkELjw9yDCpJuv9SbLSmJ4rAIS1HQwMs8rxv9Utx';
 
-
+nasaAPI();
 app.use(express.static('./app'));
-app.get('/nasa',function(req,res){
-  const option = {
-    'host':'api.nasa.gov',
-    'path':'/planetary/earth/imagery?lon=' + req.query.lon + '&lat=' + req.query.lat + '&cloud_score=True&api_key=' + NASA_API_KEY
-  };
-  https.request(option,function(response){
-    var rspData = ''
-    response.on('data',function(chunk){
-      rspData += chunk;
-    });
-    response.on('end',function(){
-      res.end(JSON.parse(rspData)['url']);
-    })
-  }).end();
-});
+
 app.listen(PORT,function(){
   console.log('The server is up! Point your browser to localhost:' + PORT);
 });
