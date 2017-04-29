e26066d740a7408daf68d992a2d12eeaf3a602f1
Ercadio <vroch@edu.uwaterloo.ca>
2017-04-29 16:54:46 -0400

    modified:   .gitignore 	modified:   app/bin/linker.js 	modified:   app/bin/style.css 	modified:   app/index.html 	new file:   bin/gMaps.js 	modified:   bin/nasaAPI.js 	modified:   bin/rRoute.js 	modified:   index.js 	modified:   package.json

--- index.js
+++ index.js
@@ -3,19 +3,23 @@ const app = express();
 const fs = require('fs');
 const https = require('https');
 const http = require('http');
+const request = require('request');
 const PORT = 8080;
 
-const skywatchAPI = require('./bin/skywatchAPI');
-skywatchAPI(https,express,app);
+// const skywatchAPI = require('./bin/skywatchAPI');
+// skywatchAPI(https,express,app);
 
-const nasaAPI = require('./bin/nasaAPI');
-nasaAPI(https,express,app);
+// const nasaAPI = require('./bin/nasaAPI');
+// nasaAPI(https,express,app);
+const gMaps = require('./bin/gMaps');
+gMaps(express,app,request);
 
 const gRadiation = require('./bin/gRadiation');
 gRadiation(https,express,app);
 
 const rRoute = require('./bin/rRoute');
 rRoute(https,express,app);
+
 
 app.use(express.static('./app'));
 
