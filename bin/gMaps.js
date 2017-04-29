bcf4b23648651ae528723b44130cbeb540ce4a81
Ercadio <vroch@edu.uwaterloo.ca>
2017-04-29 17:05:57 -0400

    modified:   bin/gMaps.js

--- bin/gMaps.js
+++ bin/gMaps.js
@@ -2,7 +2,7 @@
 var key = require('../apiKEYS.json');
 function init(express,app,request){
   app.use('/gMaps',function(req,res){
-    request('https://maps.googleapis.com/maps/api/js?key=AIzaSyCQFuPUg1TkMzG5uEvXhfIOad9M3Okb2PQ' + '&callback=initMap',function(error,response,body){
+    request('https://maps.googleapis.com/maps/api/js?key=' + key['gMaps'] + '&callback=initMap',function(error,response,body){
       res.end(body);
     });
   });
